1c8fc3cba79c7dfc15517bb3c10823fb76bd6cc6
menomi <melattekle@protonmail.com>
2021-10-05 16:17:34 +0300

    create function that creates almost anything

--- _printf.c
+++ _printf.c
@@ -37,3 +37,46 @@ static int (*check_for_specifiers(const char *format))(va_list)
 	return (pt[i].f);
 }
 
+/**
+ * _printf - prints almost anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, num = 0;
+	va_list valist;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			num++;
+		}
+		if (!format[i])
+			return (num);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			num += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		num++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (num);
+}
