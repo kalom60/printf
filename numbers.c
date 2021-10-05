70b5bd15cbdb35d54e51fc98348d22eff1407d03
kalom60 <kaleabtesfaye00@gmail.com>
2021-10-05 06:33:42 -0700

    handle d and i

--- numbers.c
+++ numbers.c
@@ -38,3 +38,44 @@ int print_d(va_list d)
 	}
 	return (num);
 }
+
+/**
+ * print_i - prints an integer
+ * @i: integer to print
+ *
+ * Return: number of chars and digits printed
+ */
+int print_i(va_list i)
+{
+	int a[10];
+	int total, num;
+	int j, m, n;
+
+	n = va_arg(i, int);
+	num = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		num++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, total = 0; j < 10; j++)
+	{
+		total += a[j];
+		if (total != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			num++;
+		}
+	}
+	return (num);
+}
+
