54240455c89d56f8e2886090e5c1d351f98f2fec
kalom60 <kaleabtesfaye00@gmail.com>
2021-10-05 07:11:07 -0700

    handle x, X and p

--- hex.c
+++ hex.c
@@ -58,7 +58,7 @@ int print_x(va_list x)
  */
 int print_X(va_list X)
 {
-  return (print_hex(va_arg(X, unsigned int), 1));
+	return (print_hex(va_arg(X, unsigned int), 1));
 }
 
 /**
@@ -70,12 +70,59 @@ int print_X(va_list X)
  */
 static unsigned long _pow(unsigned int base, unsigned int exponent)
 {
-  unsigned int i;
-  unsigned long ans = base;
+	unsigned int i;
+	unsigned long ans = base;
 
-  for (i = 1; i < exponent; i++)
-  {
-    ans *= base;
-  }
-  return (ans);
+	for (i = 1; i < exponent; i++)
+		ans *= base;
+	return (ans);
+}
+
+/**
+ * print_p - prints an address
+ * @p: address to print
+ *
+ * Return: number of characters to print
+ */
+int print_p(va_list p)
+{
+	int num = 0;
+	unsigned int a[16];
+	unsigned int i, total;
+	unsigned long n, m;
+	char *str = "(nil)";
+
+	n = va_arg(p, unsigned long);
+	if (n == 0)
+	{
+		for (i = 0; str[i]; i++)
+		{
+			_putchar(str[i]);
+			num++;
+		}
+		return (num);
+	}
+	_putchar('0');
+	_putchar('x');
+	num = 2;
+	m = _pow(16, 15); /* 16 ^ 15 */
+	a[0] = n / m;
+	for (i = 1; i < 16; i++)
+	{
+		m /= 16;
+		a[i] = (n / m) % 16;
+	}
+	for (i = 0, total = 0; i < 16; i++)
+	{
+		total += a[i];
+		if (total || i == 15)
+		{
+			if (a[i] < 10)
+				_putchar('0' + a[i]);
+			else
+				_putchar('0' + ('a' - ':') + a[i]);
+			num++;
+		}
+	}
+	return (num);
 }
